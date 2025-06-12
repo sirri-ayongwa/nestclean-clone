0deefad6ab90a2404b903f5339cc37f0c1f9af56
Sirri Ayongwa <sirribongnwi7@gmail.com>
2025-06-13 04:14:53 +0600

    final changes

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  const modal = document.getElementById("industryModal");
+  const bookButtons = document.querySelectorAll(".book-btn, .btn-hero, .footer-btn, .book-button, .cta-btn");
+  const closeBtn = document.querySelector(".modal .close");
+//   btn-hero book
+
+  bookButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
